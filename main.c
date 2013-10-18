94d7e20edd031deab43630896d1985811470b25f
unknown <C:\Users\\AppData\Roaming\The Bat!>
2013-10-18 09:16:31 +0300

    Add common file with macrodefination

--- main.c
+++ main.c
@@ -1,17 +1,4 @@
-#include <Atmel/AT892051.h>
-//#include <htc.h>
-			
-#define ClrScreen   		0x01		// 0b00000001
-#define Command 			0
-#define Data 				1
-
-#define true 1
-#define false 0
-#define Clock4015 P1_2  
-#define CmdDtaLcd P1_3				//Registr Select  0 --> registr command, 1 --> registr data  
-#define StrobLcd P1_4				// Chip select 4015 --> strobe exec for 1 -> 0
-#define Dta4015InLo P3_4
-#define Dta4015InHi P1_3
+#include <MacroDef.h>
 
 void DelayuS(unsigned char);
 void DelaymS(unsigned char);
